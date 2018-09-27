ae9ac1af6d54616646b001e5da7143ac7431ec44
Jun Zhang <zhangjunphy@gmail.com>
2018-09-27 20:36:42 +0800

    update ghc version, remove unused files

--- build.sh
+++ build.sh
@@ -1,54 +1,14 @@
 #!/bin/bash -eu
-# build.sh -- Athena build script
-# Copyright (C) 2013, 2014  Benjamin Barenblat <bbaren@mit.edu>
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the MIT (X11) License as described in the LICENSE file.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the X11 license for more details.
 
-# IMPORTANT:
-# if you have changed a file in the base directory or in the `alex/` directory,
-# running this build script will not guarantee its recompilation. run the
-# command `rm -r dist` before running this script to guarantee that everything
-# is recompiled.
-
-declare -r GHC_VERSION=7.8.2
 declare -r TOP="$(git rev-parse --show-toplevel)"
-
 declare -r GREP="grep --quiet --no-messages"
 
-function have {
-    type "$1" &>/dev/null
-}
-
 cd "$TOP"
-eval $(attach -Padd -b -f ghc)
 
-if [[ ! -d .cabal-sandbox ]]; then
-    # No Cabal sandbox yet.  Set one up.
-    cabal sandbox init
-fi
+stack solver
+stack exec alex -- --ghc --template=$TOP/alex $TOP/src/Scanner.x
+# Inform happy to create the state diagram file and print debug information
+stack exec happy -- -i -a -d $TOP/src/Parser.y
+stack build
 
-PATH="$TOP"/.cabal-sandbox/bin:"$PATH"
-
-for package in alex happy; do
-    if ! have $package; then
-	cabal install $package \
-	    -j \
-	    --enable-library-profiling --disable-executable-profiling \
-	    --enable-optimization
-    fi
-done
-
-cabal install \
-    --enable-library-profiling --enable-executable-profiling \
-    --alex-options="--ghc --template=\"$TOP/alex\"" #\
-#   --happy-options="-i -a -d"
-# uncomment the trailing '\' on line 42 and the entirety of line 43 to add
-# debug info to happy. the '-i' flag outputs a state diagram of the compiled
-# parser into the file `src/Parser.info`, and the '-a -d' flag combo tells
-# happy to print state transitions and actions taken during a parse to stderr.
-# They are most useful when used together.
+rm -f $TOP/src/Scanner.hs $TOP/src/Parser.hs
--- make-tarball.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# make-tarball.sh -- Athena script to build a tarball for project handin
-# Copyright (C) 2013 Cam Tenny <cjtenny@mit.edu>
-
-cd $(git rev-parse --show-toplevel)
-add -f ghc
-cabal clean
-rm -f $USER-handin.tar.gz
-tar --transform "s,^\./,$USER-handin/," --exclude '*.tar' --exclude '*.tar.gz' --exclude '*.git/*' --exclude '*~' -czf $USER-handin.tar.gz ./*

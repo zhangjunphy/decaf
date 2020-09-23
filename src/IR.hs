bf532d9a9d0a2dd31b824434e3730189c5463762
Jun Zhang <zhangjunphy@gmail.com>
2020-09-23 22:56:22 +0800

    merge semantic analysis and ir generation into a single pass

--- src/IR.hs
+++ src/IR.hs
@@ -29,11 +29,15 @@ module IR ( generate
           , Type
           ) where
 
+import           Control.Monad.Except
 import           Control.Monad.State
+import           Control.Monad.State.Lazy
+import           Control.Monad.Writer.Lazy
 import           Data.Foldable
+import           Data.Map                  (Map)
 import qualified Data.Map                  as Map
 import           Data.Maybe
-import           Text.Read
+import           Text.Printf               (printf)
 
 import           Data.ByteString.Lazy      (ByteString)
 import qualified Data.ByteString.Lazy      as B
@@ -42,6 +46,7 @@ import qualified Data.ByteString.Lazy.UTF8 as B
 import qualified Parser                    as P
 
 type Name = ByteString
+type ScopeID = Int
 
 -- operators
 data BinaryOp =
@@ -166,26 +171,224 @@ data Expr = LocationExpr { location :: Location }
           | LengthExpr { name :: Name }
           deriving Show
 
-data Block = Block { vars  :: [FieldDecl]
-                   , stats :: [Statement]
+data Block = Block { vars    :: [FieldDecl]
+                   , stats   :: [Statement]
+                   , blockID :: ScopeID
                    } deriving Show
+
+---------------------------------------
+-- Semantic informations and errors
+---------------------------------------
+
+-- semantic errors
+-- these errors are produced during semantic analysis,
+-- we try to detect as many as we can in a single pass
+newtype SemanticError = SemanticError ByteString
+  deriving (Show)
+
+-- exceptions during semantic analysis
+-- difference from SemanticError:
+-- whenever an exception is raised, the analysis procedure will be aborted.
+newtype SemanticException = SemanticException ByteString
+  deriving (Show)
+
+-- symbol table definitions
+data SymbolTable = SymbolTable { scopeID   :: ScopeID
+                               , parent    :: Maybe SymbolTable
+                               , importSymbols   :: Maybe (Map IR.Name IR.ImportDecl)
+                               , variableSymbols :: Map IR.Name IR.FieldDecl
+                               , methodSymbols   :: Maybe (Map IR.Name IR.MethodDecl)
+                               } deriving Show
+data SemanticState = SemanticState
+  { nextScopeID    :: ScopeID
+  , currentScopeID :: ScopeID
+  , symbolTables   :: Map ScopeID SymbolTable
+  } deriving Show
+
+-- Monad used for semantic analysis
+-- Symbol tables are built for every scope, and stored in SemanticState.
+-- Semantic errors encountered are recorded by the writer monad (WriterT [SemanticError]).
+-- If a serious problem happened such that the analysis has to be aborted, a SemanticException
+-- is thrown.
+newtype Semantic a = Semantic { runSemantic :: ExceptT SemanticException (WriterT [SemanticError] (State SemanticState))  a }
+  deriving (Functor, Applicative, Monad, MonadError SemanticException, MonadWriter [SemanticError], MonadState SemanticState)
+
+throwSemanticException :: String -> Semantic a
+throwSemanticException msg = throwError $ SemanticException $ B.fromString msg
+
+addSemanticError :: String -> Semantic ()
+addSemanticError msg = tell $ [ SemanticError (B.fromString msg) ]
+
+-- find symbol table for current scope
+getSymbolTable :: Semantic (Maybe SymbolTable)
+getSymbolTable = do
+  state <- get
+  let id = currentScopeID state
+  return $ Map.lookup id $ symbolTables state
+
+getCurrentScopeID :: Semantic Int
+getCurrentScopeID = do
+  state <- get
+  return $ currentScopeID state
+
+-- find symbol table for current scope
+-- will throw SemanticException if nothing is found
+getSymbolTable' :: Semantic SymbolTable
+getSymbolTable' = do
+  scopeID <- getCurrentScopeID
+  t <- getSymbolTable
+  case t of
+    Nothing    -> throwSemanticException $
+      printf "No symble table found for current scope %d" $ scopeID
+    Just table -> return table
+
+getLocalVariables' :: Semantic (Map IR.Name IR.FieldDecl)
+getLocalVariables' = do
+  localST <- getSymbolTable'
+  return $ variableSymbols localST
+
+getLocalImports' :: Semantic (Map IR.Name IR.ImportDecl)
+getLocalImports' = do
+  localST <- getSymbolTable'
+  case importSymbols localST of
+    Nothing -> throwSemanticException $ printf "No import table for scope %d" $ scopeID localST
+    Just t -> return t
+
+getLocalMethods' :: Semantic (Map IR.Name IR.MethodDecl)
+getLocalMethods' = do
+  localST <- getSymbolTable'
+  case methodSymbols localST of
+    Nothing -> throwSemanticException $ printf "No method table for scope %d" $ scopeID localST
+    Just t  -> return t
+
+updateSymbolTable :: SymbolTable -> Semantic ()
+updateSymbolTable t = do
+  state <- get
+  -- ensure the symbol table is present, otherwise throw an exception
+  getSymbolTable'
+  put $ state { symbolTables = Map.insert (currentScopeID state) t (symbolTables state) }
+
+-- lookup symbol in local scope.
+-- this is useful to find naming conflicts.
+lookupLocalVariable :: IR.Name -> Semantic (Maybe IR.FieldDecl)
+lookupLocalVariable name = do
+  table <- getSymbolTable
+  return $ table >>= (Map.lookup name . variableSymbols)
+
+lookupVariable :: IR.Name -> Semantic (Maybe IR.FieldDecl)
+lookupVariable name = do
+  local <- getSymbolTable
+  return $ local >>= lookup name
+  where lookup name table = case Map.lookup name (variableSymbols table) of
+          -- found in local symbol table
+          Just s -> Just s
+          -- otherwise search in parent table
+          Nothing -> case (parent table) of
+                       Nothing -> Nothing
+                       Just p  -> lookup name p
+
+enterScope :: IR.Block -> Semantic ()
+enterScope block = do
+  state <- get
+  parentST <- getSymbolTable
+  let currentID = currentScopeID state
+      nextID = nextScopeID state
+      localST = SymbolTable { scopeID = nextID
+                            , parent = parentST
+                            , variableSymbols = Map.empty
+                            , importSymbols = Nothing
+                            , methodSymbols = Nothing
+                            }
+  put $ state { nextScopeID = nextID + 1
+              , currentScopeID = nextID
+              , symbolTables = Map.insert nextID localST $ symbolTables state
+              }
+
+exitScope :: Semantic ()
+exitScope = do
+  state <- get
+  localST <- getSymbolTable
+  case localST of
+    Nothing ->
+      throwSemanticException
+        $ printf "No symbol table is associated with scope(%d)!" $ currentScopeID state
+    Just table ->
+      case parent table of
+        Nothing ->
+          throwSemanticException "Cannot exit root scope!"
+        Just p ->
+          put $ state { currentScopeID = scopeID p }
+
+addVariableDef :: IR.FieldDecl -> Semantic ()
+addVariableDef def = do
+  localST <- getSymbolTable'
+  -- check for duplications
+  let nm = IR.name (def :: IR.FieldDecl)
+  case Map.lookup (IR.name (def :: IR.FieldDecl)) (variableSymbols localST) of
+    Just _ -> addSemanticError $ printf "duplicate definition for variable %d" $ B.toString nm
+  let variableSymbols' = Map.insert (IR.name (def :: IR.FieldDecl)) def (variableSymbols localST)
+      newST = localST { variableSymbols = variableSymbols' }
+  -- ensure declared array size > 0
+  case IR.size def of
+    Just s -> if s > 0 then addSemanticError "" else tell []
+  updateSymbolTable newST
+
+addImportDef :: IR.ImportDecl -> Semantic ()
+addImportDef def = do
+  localST <- getSymbolTable'
+  importTable <- getLocalImports'
+  let nm = IR.name (def :: IR.ImportDecl)
+  case Map.lookup (IR.name (def :: IR.ImportDecl)) importTable of
+    Just _ -> addSemanticError $ printf "duplicate import %d" $ B.toString nm
+  let importSymbols' = Map.insert (IR.name (def :: IR.ImportDecl)) def importTable
+      newST = localST { importSymbols = Just importSymbols' }
+  updateSymbolTable newST
+
+addMethodDef :: IR.MethodDecl -> Semantic ()
+addMethodDef def = do
+  localST <- getSymbolTable'
+  methodTable <- getLocalMethods'
+  let nm = IR.name (def :: IR.MethodDecl)
+  case Map.lookup (IR.name (def :: IR.MethodDecl)) methodTable of
+    Just _ -> addSemanticError $ printf "duplicate definition for method %d" $ B.toString nm
+  let methodSymbols' = Map.insert (IR.name (def :: IR.MethodDecl)) def methodTable
+      newST = localST { methodSymbols = Just methodSymbols' }
+  updateSymbolTable newST
+
+-- traverse ir tree to compose symbol tables and check semantic errors
+semanticAnalysis :: IR.IRRoot -> Semantic ()
+semanticAnalysis (IR.IRRoot imports vars methods) = do
+  -- initial state
+  let globalST = SymbolTable { scopeID = 0
+                             , parent = Nothing
+                             , importSymbols = Just Map.empty
+                             , variableSymbols = Map.empty
+                             , methodSymbols = Just Map.empty
+                             }
+  put SemanticState { nextScopeID = 1
+                    , currentScopeID = 0
+                    , symbolTables = Map.fromList [(0, globalST)]
+                    }
+  -- add imports to symbol table
+  _
 
 ----------------------------------------------------------------------
 -- Convert the parser tree into an IR tree
+-- Generate symbol tables at the same time.
+-- Also detects semantic errors.
 ----------------------------------------------------------------------
 
-generate :: P.Program -> IRRoot
-generate (P.Program imports fields methods) = IRRoot
-  (irgenImportDecl <$> imports)
-  (concat $ irgenFieldDecl <$> fields)
-  (irgenMethodDecl <$> methods)
+generate :: P.Program -> Semantic IRRoot
+generate (P.Program imports fields methods) = do
+  imports <- irgenImportDecl <$> imports
+  _
 
 irgenType :: P.Type -> Type
 irgenType P.IntType  = IntType
 irgenType P.BoolType = BoolType
 
-irgenImportDecl :: P.ImportDecl -> ImportDecl
-irgenImportDecl (P.ImportDecl id) = ImportDecl id
+irgenImportDecl :: P.ImportDecl -> Semantic ImportDecl
+irgenImportDecl (P.ImportDecl id) = return $ ImportDecl id
 
 irgenFieldDecl :: P.FieldDecl -> [FieldDecl]
 irgenFieldDecl (P.FieldDecl tpe elems) =
@@ -202,7 +405,7 @@ irgenMethodDecl (P.MethodDecl id returnType arguments block) =
   where
     args = map (\(P.Argument id tpe) -> (id, irgenType tpe)) arguments
 
-irgenBlock :: P.Block -> Block
+irgenBlock :: P.Block -> Semantic Block
 irgenBlock (P.Block fieldDecls statements) = Block fields stmts
   where
     fields = concat $ irgenFieldDecl <$> fieldDecls
